48d42a52eaafccb05c3a65207e8c6b74d31897ee
CR-SCNT <nmksva@gmail.com>
2025-05-28 21:03:25 +0800

    Add server configuration and main logic for video segmentation

---
+++ server/server_main.py
@@ -0,0 +1,59 @@
+import segmenter
+import socket
+import os
+import glob
+import concurrent.futures
+import subprocess
+import sys
+from sender import recv_and_send as tcp_communicate
+from config import HOST, PORT, BUFFER_SIZE, SEGMENT_DIR, INPUT_DIR, OUTPUT_DIR, PROFILES, DURATION
+
+def check_ffmpeg():
+    try:
+        subprocess.run(['ffmpeg', '-version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        print("[√] ffmpeg available.")
+    except Exception:
+        print("[×] ffmpeg not found. Please install ffmpeg and ensure it is in your PATH.")
+        sys.exit(1)
+
+def check_and_segment():
+    
+    for video in glob.glob(os.path.join(INPUT_DIR, "*.mp4")):
+        video_name = os.path.splitext(os.path.basename(video))[0]
+        segment_path = os.path.join(SEGMENT_DIR, video_name)
+        
+        if not os.path.exists(segment_path) or not os.listdir(segment_path):
+            print(f"[!] Segments for video '{video_name}' do not exist, starting segmentation.")
+            for res_label, res_size, bitrate in PROFILES:
+                segmenter.segment_video(
+                    res_label=res_label,
+                    resolution_size=res_size,
+                    bitrate_kbps=bitrate,
+                    input_dir=INPUT_DIR,
+                    output_dir=OUTPUT_DIR,
+                    video_name=video_name,
+                    duration=DURATION
+                )
+
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.listen(100)
+    print(f"[+] Server started on {HOST}:{PORT}")
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        try:
+            while True:
+                client_socket, client_address = server_socket.accept()
+                executor.submit(tcp_communicate, client_socket, client_address, BUFFER_SIZE, SEGMENT_DIR)
+        except KeyboardInterrupt:
+            print("\n[!] Server shutting down by keyboard interrupt.")
+        finally:
+            server_socket.close()
+            print("[!] Server socket closed.")
+            
+if __name__ == "__main__":
+    check_ffmpeg()
+    check_and_segment()
+    start_server()
