3c7ef1787fa8f29fb7027c171b354c1384d0d3d9
CR-SCNT <nmksva@gmail.com>
2025-06-04 15:04:53 +0800

    add new function to choose video on server

--- client/client_main.py
+++ client/client_main.py
@@ -13,17 +13,28 @@ def start_client():
         if prefix == b'KEY:':
             key = client_socket.recv(16)
         with open('./client/aes.key', 'wb') as key_file:
-            key_file.write(key)
+                key_file.write(key)
         print("[√] Key received and saved as 'aes.key'.")
-        
-        namedata = client_socket.recv(BUFFER_SIZE)
-        namedata = str(namedata, encoding='utf-8')
-        print(str(namedata))
-        receiver.suggest_recv(client_socket, BUFFER_SIZE, namedata, player)
-    except KeyboardInterrupt:
-        print("\n[!] Server shutting down by keyboard interrupt.")
-    finally:
+    except Exception as e:
+        print(f"[!] Error receiving key: {e}")
         client_socket.close()
+        return
+    while True:
+        try:
+            namedata = client_socket.recv(BUFFER_SIZE)
+            namedata = str(namedata, encoding="utf8")
+            print("Available videos on server:")
+            print(namedata)
+            videoname = input("Enter video name: ")
+            if videoname not in namedata:
+                print(f"[!] Video '{videoname}' not found on server.")
+                continue    
+            # resolution set ["1080p", "2500k"] by default, abr_control will be implemented later
+            receiver.suggest_recv(client_socket, BUFFER_SIZE, videoname, player,["1080p", "2500k"]) 
+        except KeyboardInterrupt:
+            print("\n[!] Server shutting down by keyboard interrupt.")
+        finally:
+            client_socket.close()
 
 if __name__ == "__main__":
     start_client()
--- client/receiver.py
+++ client/receiver.py
@@ -6,40 +6,31 @@ from config import DOWNLOAD_DIR
 def filename_summon(name, resolution, ords):
     return f"{name}-{resolution[0]}-{resolution[1]}-" + str(ords).zfill(4) + ".ts"
 
-def suggest_recv(client_socket, buffer_size, video_names, videoplayer):
-    name = video_names.split()
-    for nm in name:
-        nm = str(nm)
-    resolution = [["480p", "1000k"], ["720p", "1500k"], ["1080p", "2500k"], ["360p", "500k"]]
+def suggest_recv(client_socket, buffer_size, video_name, videoplayer, resolution):
+    # resolution = [["480p", "1000k"], ["720p", "1500k"], ["1080p", "2500k"], ["360p", "500k"]]
     ords = 0
-    for video in range(0, len(name)):
-        vd = name[video]
-        for od in range(0,4):
-            ords = 0
-            reso = resolution[od]
-            while True:
-                strs = filename_summon(vd, reso, ords)
-                encrypted_strs = strs + ".aes"
-                client_socket.sendall(bytes(strs, encoding="utf8"))
-                time.sleep(2)
-                with open(os.path.join(DOWNLOAD_DIR, encrypted_strs), 'wb') as f:
-                    write_data = b""
+    while True:
+        strs = filename_summon(video_name, resolution, ords)
+        encrypted_strs = strs + ".aes"
+        client_socket.sendall(bytes(strs, encoding="utf8"))
+        time.sleep(2)
+        with open(os.path.join(DOWNLOAD_DIR, encrypted_strs), 'wb') as f:
+            write_data = b""
+            data = client_socket.recv(buffer_size)
+            if data == b"Invalid segment name format." or data == b"Segment not found.":
+                break
+            elif data:
+                #subprocess.Popen(["ffmpeg", "-i", "tcp://localhost:8000", "-c", "copy", "-f", ".ts", "data/download/" + strs], stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+                client_socket.send(b"ACK")
+                write_data += data
+                while True:
                     data = client_socket.recv(buffer_size)
-                    if data == b"Invalid segment name format." or data == b"Segment not found.":
-                        #os.remove("data/download/" + strs)
+                    if not data or data == b"END":
                         break
-                    elif data:
-                        #subprocess.Popen(["ffmpeg", "-i", "tcp://localhost:8000", "-c", "copy", "-f", ".ts", "data/download/" + strs], stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
-                        client_socket.send(b"ACK")
-                        write_data += data
-                        while True:
-                            data = client_socket.recv(buffer_size)
-                            if not data or data == b"END":
-                                break
-                            write_data += data
-                            client_socket.send(b"ACK")
-                            print(f"{len(write_data)}, chunk acquire succeeded")
-                        print("Data collect completed")
-                        f.write(write_data)
-                videoplayer.add_playlist(DOWNLOAD_DIR + strs)
-                ords += 1
+                    write_data += data
+                    client_socket.send(b"ACK")
+                    print(f"{len(write_data)}, chunk acquire succeeded")
+                print("Data collect completed")
+                f.write(write_data)
+            videoplayer.add_playlist(DOWNLOAD_DIR + strs)
+            ords += 1
